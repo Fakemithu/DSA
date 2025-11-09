65fc2904e8805e788a7b82276c0b9eba643f1ee5
Fakemithu <shukla.sudhansu18@gmail.com>
2025-11-09 18:58:59 +0530

    Time: 0 ms (100.00%) | Memory: 31.5 MB (8.88%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -6,7 +6,7 @@ public:
         int start = 0, end = n-1, mid;
 
         while(start <= end){
-            mid = (start+end)/2;
+            mid = start + (end - start)/2;
 
             if(nums[mid] == target){
                 return mid;
