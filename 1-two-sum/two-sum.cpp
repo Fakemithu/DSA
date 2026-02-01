60c1312ea32d90a3d7619cc84f222f75a6f718c4
Fakemithu <shukla.sudhansu18@gmail.com>
2026-02-01 11:15:25 +0530

    Time: 116 ms (6.25%) | Memory: 14.1 MB (66.45%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -9,7 +9,8 @@ public:
             for(int j=i+1; j<n; j++){
                 if(nums[i] + nums[j] == target){
                   ans.push_back(i);
-                  ans.push_back(j);  
+                  ans.push_back(j);
+                  break;  
                 }
             }
         }
