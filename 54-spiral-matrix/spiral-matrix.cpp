39019d94afe9c52ba3a806676dcf7db4b9656080
Fakemithu <shukla.sudhansu18@gmail.com>
2026-07-01 15:35:58 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (81.37%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -9,21 +9,27 @@ public:
         int top = 0, bottom = row-1, left = 0, right = col-1;
 
         while(top<=bottom && left<=right){
+            //print top
             for(int j=left; j<=right; j++){
                 ans.push_back(matrix[top][j]);
             }
             top++;
 
+            //print right
             for(int i=top; i<=bottom; i++){
                 ans.push_back(matrix[i][right]);
             }
             right--;
+
+            //print bottom
             if(top<=bottom){
                 for(int j=right; j>=left; j--){
                     ans.push_back(matrix[bottom][j]);
                 }
                 bottom--;
             }
+
+            //print left
             if(left<=right){
                 for(int i=bottom; i>=top; i--){
                     ans.push_back(matrix[i][left]);
